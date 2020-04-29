5c8cc67cb48f23e5e06af16227dc3d1ddba6fd53
Oleksandr Butrym <butrym@karazin.ua>
2020-04-29 22:12:37 +0300

    додав коментарів

--- sprint9/task4/Question4.java
+++ sprint9/task4/Question4.java
@@ -9,8 +9,8 @@ public class MyUtils {
     if (originalText.isEmpty()) {
       return modifyText.toUpperCase();
 
-    } else if (modifyText.isEmpty()) {
-      return modifyText.toUpperCase();
+    } else if (modifyText.isEmpty()) { // можна не писати else, бо попередня гілка завершується з return
+      return modifyText.toUpperCase(); // в чому сенс змінювати case порожнього рядку?
     }
 
     ArrayList<String> originalTextSplit = new ArrayList<>();
@@ -25,17 +25,19 @@ public class MyUtils {
 
       if (originalTextSplit.get(0).equals(modifyTextSplit.get(0))) {
 
-        checkedString += originalTextSplit.get(0) + " ";
+        checkedString += originalTextSplit.get(0) + " "; // StringBuilder для додавання в кінець
+                                                    // тут можуть з'являтись зайві пробіли
 
-        originalTextSplit.remove(0);
-        modifyTextSplit.remove(0);
+        originalTextSplit.remove(0); // ArrayList не підходить для операцій видалення с початку, бо при цьому 
+        modifyTextSplit.remove(0);  // всі елементи копіюються ліворуч. Для того є інтерфейс Deque та імплементація ArrayDeque
+                                    // якщо ж треба лише видаляти з початку, а додавати с кінця - то Queue
       } else {
-        checkedString += modifyTextSplit.get(0).toUpperCase() + " ";
-        modifyTextSplit.remove(0);
+        checkedString += modifyTextSplit.get(0).toUpperCase() + " "; // тут можуть з'являтись зайві пробіли
+        modifyTextSplit.remove(0); // замість видалення з голови можна зсувати змінну-індекс вздовж масива
       }
     }
     return modifyTextSplit.isEmpty()
         ? checkedString.substring(0, checkedString.length() - 1)
-        : checkedString + modifyTextSplit.get(0).toUpperCase();
+        : checkedString + modifyTextSplit.get(0).toUpperCase(); // чому лише одне слово, а якщо там кілька доданих слів лишилось?
   }
 }
