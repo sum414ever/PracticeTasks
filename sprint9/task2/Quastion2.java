1f8d0d8e1baa6fd970f6a48fa84c20a1bbf67db9
Oleksandr Butrym <butrym@karazin.ua>
2020-04-29 20:24:16 +0300

    referenced the algorithm explanation

--- sprint9/task2/Quastion2.java
+++ sprint9/task2/Quastion2.java
@@ -2,7 +2,8 @@ public class MyUtils {
 
   public boolean verifyBrackets(String text) {
     text = text.replaceAll("\\\\[\\Q[](){}\\E]", "");
-
+    // це класичний алгоритм на застосування структури даних стек.
+    // подивись https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
     if (text.length() % 2 != 0) {
       return false;
     } else {
@@ -10,10 +11,10 @@ public class MyUtils {
       int size = 0;
       do {
         size = text.length();
-        text = text.replace("{}", "");
-        text = text.replace("()", "");
-        text = text.replace("[]", "");
-      } while (size != text.length());
+        text = text.replace("{}", ""); // це працюватиме лише якщо там нема інших символів
+        text = text.replace("()", ""); // до того ж для довгого рядка за одну ітерацію буде
+        text = text.replace("[]", ""); // видалятись лише по два символи і створюватиметься
+      } while (size != text.length()); // новий рядок. Тому ефективність буде квадратичною O(n^2)
 
       return text.length() == 0;
     }
