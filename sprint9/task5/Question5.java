906ce1b16a9fecf6728e475ea84f89fc44529bc3
sum414ever <copokaalekc@gmail.com>
2020-04-30 13:00:45 +0300

    Fixed remarks in Question5 by AButrym

--- sprint9/task5/Question5.java
+++ sprint9/task5/Question5.java
@@ -1,20 +1,20 @@
 public class MyUtils {
 
+  static final int MAX_LENGTH = 60;
+
   public String reformatLines(String text) {
-    text = text.replaceAll("\n", " ");  // тут, можливо, помилка, бо replaceAll працює з регексом, а там новий рядок задається як \n
-                                        // але, щоб він став таким, в java-рядку бекслеш треба подвоювати
-    text = text.replaceAll("\\s+", " "); // класс \\s включає до себе й символ нового рядку
+    text = text.replaceAll("\\s+", " ");
 
     String ret = "";
     String meta = "";
 
-    String[] s = text.trim().split(" "); // назви змінних мають бути читабельні, words було б OK
-    for (String d : s) { // те саме, краще було б назвати for (String word : words)
-      if (meta.length() + d.length() <= 60) { // константі 60 краще дати назву та винести в static final
-        meta += d + " ";
+    String[] words = text.trim().split(" ");
+    for (String word : words) {
+      if (meta.length() + word.length() <= MAX_LENGTH) {
+        meta += word + " ";
       } else {
         ret += meta.substring(0, meta.length() - 1) + "\n";
-        meta = d + " ";
+        meta = word + " ";
       }
     }
     return ret + meta.substring(0, meta.length() - 1);
